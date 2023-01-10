36a046c577ccd50801d0f23b16865d94ff29b31c
Erich Scheuch <erich.scheuch@googlemail.com>
2023-01-10 13:20:33 +0100

    Neue Azure Linie hinzugefügt

--- microsoft_cloud_connetion.ps1
+++ microsoft_cloud_connetion.ps1
@@ -1,3 +1,11 @@
+Get-ExecutionPolicy -List
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
 Install-Module -Name Az -AllowClobber -Verbose -Force
 
 Import-Module -Name Az
+
+Connect-AzAccount -TenantId f195a009-8edf-4e13-b15d-ea6d8a775027
+
+Get-AzSubscription
